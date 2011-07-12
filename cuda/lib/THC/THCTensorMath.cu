697fa533acf52e078e9a1242d78d62fdaa6e3cc9
Ronan Collobert <ronan@collobert.com>
2011-07-12 15:38:00 +0200

    more on THCTensorMath

--- cuda/lib/THC/THCTensorMath.cu
+++ cuda/lib/THC/THCTensorMath.cu
@@ -249,3 +249,176 @@ float THCudaTensor_sum(THCudaTensor *self)
   return result;
 }
 
+void THCudaTensor_addmv(THCudaTensor *self_, float alpha, THCudaTensor *mat, THCudaTensor *vec)
+{
+  if( (mat->nDimension != 2) || (vec->nDimension != 1) )
+    THError("matrix and vector expected");
+ 
+  if( mat->size[1] != vec->size[0] )
+    THError("size mismatch");
+  
+  if(self_->nDimension != 1)
+    THError("size mismatch");
+    
+  if( self_->size[0] != mat->size[0] )
+    THError("size mismatch");
+
+  {
+    THCudaTensor *self = THCudaTensor_newContiguous(self_);
+    mat = THCudaTensor_newContiguous(mat);
+    vec = THCudaTensor_newContiguous(vec);
+
+    cublasSgemv('t',  mat->size[1], mat->size[0],
+                alpha, THCudaTensor_data(mat), mat->stride[0],
+                THCudaTensor_data(vec), vec->stride[0],
+                1, THCudaTensor_data(self), self->stride[0]);
+
+    THCudaTensor_free(mat);
+    THCudaTensor_free(vec);
+    THCudaTensor_freeCopyTo(self, self_);
+  }
+}
+
+void THCudaTensor_addmm(THCudaTensor *self_, float alpha, THCudaTensor *mat1, THCudaTensor *mat2)
+{
+  if( (mat1->nDimension != 2) || (mat2->nDimension != 2) ) 
+    THError("matrix and matrix expected"); 
+ 
+  if(self_->nDimension != 2)
+    THError("size mismatch"); 
+
+  if( (self_->size[0] != mat1->size[0]) || (self_->size[1] != mat2->size[1]) || (mat1->size[1] != mat2->size[0]) ) 
+    THError("size mismatch"); 
+
+  {
+    THCudaTensor *self = THCudaTensor_newContiguous(self_);
+    mat1 = THCudaTensor_newContiguous(mat1);
+    mat2 = THCudaTensor_newContiguous(mat2);
+
+    cublasSgemm('n',
+                'n',
+                self->size[1],
+                self->size[0],
+                mat2->size[0],
+                alpha,
+                THCudaTensor_data(mat2),
+                mat2->stride[0],
+                THCudaTensor_data(mat1),
+                mat1->stride[0],
+                1,
+                THCudaTensor_data(self),
+                self->stride[0]);
+    
+    THCudaTensor_free(mat1);
+    THCudaTensor_free(mat2);
+    THCudaTensor_freeCopyTo(self, self_);
+  }
+}
+
+void THCudaTensor_addr(THCudaTensor *self_, float alpha, THCudaTensor *vec1, THCudaTensor *vec2)
+{
+  if( (vec1->nDimension != 1) || (vec2->nDimension != 1) )
+    THError("vector and vector expected");
+
+  if(self_->nDimension != 2)
+    THError("size mismatch");
+    
+  if( (self_->size[0] != vec1->size[0]) || (self_->size[1] != vec2->size[0]) )
+    THError("size mismatch");
+
+  {
+    THCudaTensor *self = THCudaTensor_newContiguous(self_);
+    vec1 = THCudaTensor_newContiguous(vec1);
+    vec2 = THCudaTensor_newContiguous(vec2);
+
+    cublasSger(vec2->size[0], vec1->size[0],
+               alpha, THCudaTensor_data(vec2), vec2->stride[0],
+               THCudaTensor_data(vec1), vec1->stride[0],
+               THCudaTensor_data(self), self->stride[0]);
+    
+    THCudaTensor_free(vec1);
+    THCudaTensor_free(vec2);
+    THCudaTensor_freeCopyTo(self, self_);
+  }  
+}
+
+#define IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(NAME, CFUNC)                   \
+  __global__ void THCudaTensor_kernel_##NAME(float *data, long size)       \
+  {                                                                     \
+    long k = blockDim.x * blockIdx.x + threadIdx.x;                     \
+                                                                        \
+    if(k < size)                                                        \
+      data[k] = CFUNC(data[k]);                                           \
+  }                                                                     \
+                                                                        \
+  void THCudaTensor_##NAME(THCudaTensor *self_)                         \
+  {                                                                     \
+    THCudaTensor *self = THCudaTensor_newContiguous(self_);             \
+    long size = THCudaTensor_nElement(self);                            \
+    long nbBlocksPerGrid = (size + NB_THREADS_PER_BLOCK - 1) / NB_THREADS_PER_BLOCK; \
+                                                                        \
+    THCudaTensor_kernel_##NAME<<<nbBlocksPerGrid, NB_THREADS_PER_BLOCK>>>(THCudaTensor_data(self), size); \
+                                                                        \
+    THCudaTensor_freeCopyTo(self, self_);                               \
+  }
+
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(log, log)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(log1p, log1p)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(exp, exp)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(cos, cos)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(acos, acos)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(cosh, cosh)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(sin, sin)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(asin, asin)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(sinh, sinh)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(tan, tan)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(atan, atan)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(tanh, tanh)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(sqrt, sqrt)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(ceil, ceil)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(floor, floor)
+IMPLEMENT_CUDA_TENSOR_BASIC_FUNC(abs, fabs)
+
+__global__ void THCudaTensor_kernel_pow(float *data, float value, long size)
+{
+  long k = blockDim.x * blockIdx.x + threadIdx.x;
+
+  if(k < size)
+    data[k] = pow(data[k], value);
+}
+
+void THCudaTensor_pow(THCudaTensor *self_, float value)
+{
+  THCudaTensor *self = THCudaTensor_newContiguous(self_);
+  long size = THCudaTensor_nElement(self);
+  long nbBlocksPerGrid = (size + NB_THREADS_PER_BLOCK - 1) / NB_THREADS_PER_BLOCK;
+
+  THCudaTensor_kernel_pow<<<nbBlocksPerGrid, NB_THREADS_PER_BLOCK>>>(THCudaTensor_data(self), value, size);
+
+  THCudaTensor_freeCopyTo(self, self_);
+}
+
+float THCudaTensor_mean(THCudaTensor *self)
+{
+  return -1;
+}
+
+float THCudaTensor_var(THCudaTensor *self)
+{
+  return -1;
+}
+
+float THCudaTensor_std(THCudaTensor *self)
+{
+  return -1;
+}
+
+float THCudaTensor_norm(THCudaTensor *self, float value)
+{
+  return -1;
+}
+
+float THCudaTensor_dist(THCudaTensor *self, THCudaTensor *src, float value)
+{
+  return -1;
+}
