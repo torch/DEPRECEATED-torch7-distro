3e5608137a3855891331805dbec1584f3bc217b4
Ronan Collobert <ronan@collobert.com>
2011-09-07 17:47:48 +0200

    ClassNLLCriterion now handles batches

--- packages/nn/ClassNLLCriterion.lua
+++ packages/nn/ClassNLLCriterion.lua
@@ -2,16 +2,41 @@ local ClassNLLCriterion, parent = torch.class('nn.ClassNLLCriterion', 'nn.Criter
 
 function ClassNLLCriterion:__init()
    parent.__init(self)
+   self.sizeAverage = true
 end
 
 function ClassNLLCriterion:forward(input, target)
-   self.output = -input[target]
+   if input:dim() == 1 then
+      self.output = -input[target]
+   else
+      local output = 0
+      for i=1,target:size(1) do
+         output = output - input[i][target[i]]
+      end
+      if self.sizeAverage then
+         output = output / target:size(1)
+      end
+      self.output = output
+   end
    return self.output
 end
 
 function ClassNLLCriterion:backward(input, target)
    self.gradInput:resizeAs(input)
    self.gradInput:zero()
-   self.gradInput[target] = -1
+
+  if input:dim() == 1 then
+      self.gradInput[target] = -1
+   else
+      local z = -1
+      if self.sizeAverage then
+         z = z / target:size(1)
+      end
+      local gradInput = self.gradInput
+      for i=1,target:size(1) do
+         gradInput[i][target[i]] = z
+      end
+   end
+
    return self.gradInput
 end
