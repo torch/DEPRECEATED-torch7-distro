66cbf6d3fcc4968be99573648a89d3ac0aad72c1
Ronan Collobert <ronan@collobert.com>
2011-09-09 17:50:59 +0200

    TemporalConvolution now supports accGradParameters

--- cuda/pkg/cunn/TemporalConvolution.cu
+++ cuda/pkg/cunn/TemporalConvolution.cu
@@ -77,12 +77,61 @@ static int cunn_TemporalConvolution_backward(lua_State *L)
   long nOutputFrame = gradOutput->size[0];
 
   THCudaTensor *weight = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "weight", torch_CudaTensor_id);
-  THCudaTensor *gradWeight = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "gradWeight", torch_CudaTensor_id);
-  THCudaTensor *gradBias = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "gradBias", torch_CudaTensor_id);
   THCudaTensor *gradInput = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "gradInput", torch_CudaTensor_id);
 
   THCudaTensor *gradOutputWindow;
-  THCudaTensor *inputWindow, *gradInputWindow;
+  THCudaTensor *gradInputWindow;
+  long k;
+
+
+  /* Not necessary with partial backprop: */
+  gradOutputWindow = THCudaTensor_new();
+  gradInputWindow = THCudaTensor_new();
+
+  THCudaTensor_resizeAs(gradInput, input);
+  THCudaTensor_zero(gradInput);
+
+  /* ouch */
+  for(k = 0; nOutputFrame > 0; k++)
+  {
+    long outputFrameStride = (kW-1)/dW+1;
+    long inputFrameStride = outputFrameStride*dW;
+    long nFrame = (nInputFrame-k*dW-kW)/inputFrameStride + 1;
+    nOutputFrame -= nFrame;
+
+    THCudaTensor_setStorage2d(gradOutputWindow, gradOutput->storage, 
+                            gradOutput->storageOffset + k*gradOutput->size[1],
+                            nFrame, outputFrameStride*gradOutput->size[1],
+                            gradOutput->size[1], 1);
+
+    THCudaTensor_setStorage2d(gradInputWindow, gradInput->storage,
+                            gradInput->storageOffset+k*dW*gradInput->size[1],
+                            nFrame, inputFrameStride*gradInput->size[1],
+                            kW*gradInput->size[1], 1);
+
+    THCudaTensor_addmm(gradInputWindow, 1, 1, gradOutputWindow, weight);
+  }
+
+  THCudaTensor_free(gradOutputWindow);
+  THCudaTensor_free(gradInputWindow);
+
+  return 1;
+}
+
+static int cunn_TemporalConvolution_accGradParameters(lua_State *L)
+{
+  THCudaTensor *input = (THCudaTensor*)luaT_checkudata(L, 2, torch_CudaTensor_id);  
+  THCudaTensor *gradOutput = (THCudaTensor*)luaT_checkudata(L, 3, torch_CudaTensor_id);  
+  int kW = luaT_getfieldcheckint(L, 1, "kW");
+  int dW = luaT_getfieldcheckint(L, 1, "dW");
+  long nInputFrame = input->size[0];
+  long nOutputFrame = gradOutput->size[0];
+
+  THCudaTensor *gradWeight = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "gradWeight", torch_CudaTensor_id);
+  THCudaTensor *gradBias = (THCudaTensor*)luaT_getfieldcheckudata(L, 1, "gradBias", torch_CudaTensor_id);
+
+  THCudaTensor *gradOutputWindow;
+  THCudaTensor *inputWindow;
   long k;
 
 
@@ -90,10 +139,6 @@ static int cunn_TemporalConvolution_backward(lua_State *L)
   input = THCudaTensor_newContiguous(input);
   gradOutputWindow = THCudaTensor_new();
   inputWindow = THCudaTensor_new();
-  gradInputWindow = THCudaTensor_new();
-
-  THCudaTensor_resizeAs(gradInput, input);
-  THCudaTensor_zero(gradInput);
 
   /* bias first */
   for(k = 0; k < nOutputFrame; k++)
@@ -110,8 +155,6 @@ static int cunn_TemporalConvolution_backward(lua_State *L)
     long nFrame = (nInputFrame-k*dW-kW)/inputFrameStride + 1;
     nOutputFrame -= nFrame;
 
-    /* ------------------------- gradWeight ------------------------------------- */
-
     THCudaTensor_setStorage2d(inputWindow, input->storage,
                             input->storageOffset+k*dW*input->size[1],
                             nFrame, inputFrameStride*input->size[1],
@@ -125,20 +168,10 @@ static int cunn_TemporalConvolution_backward(lua_State *L)
     THCudaTensor_transpose(gradOutputWindow, NULL, 0, 1);
     THCudaTensor_addmm(gradWeight, 1, 1, gradOutputWindow, inputWindow);
     THCudaTensor_transpose(gradOutputWindow, NULL, 0, 1);
-
-    /* -------------------------- gradInput ------------------------------------- */
-
-    THCudaTensor_setStorage2d(gradInputWindow, gradInput->storage,
-                            gradInput->storageOffset+k*dW*gradInput->size[1],
-                            nFrame, inputFrameStride*gradInput->size[1],
-                            kW*gradInput->size[1], 1);
-
-    THCudaTensor_addmm(gradInputWindow, 1, 1, gradOutputWindow, weight);
   }
 
   THCudaTensor_free(gradOutputWindow);
   THCudaTensor_free(inputWindow);
-  THCudaTensor_free(gradInputWindow);
   THCudaTensor_free(input);
 
   return 1;
@@ -147,6 +180,7 @@ static int cunn_TemporalConvolution_backward(lua_State *L)
 static const struct luaL_Reg cunn_TemporalConvolution__ [] = {
   {"TemporalConvolution_forward", cunn_TemporalConvolution_forward},
   {"TemporalConvolution_backward", cunn_TemporalConvolution_backward},
+  {"TemporalConvolution_accGradParameters", cunn_TemporalConvolution_accGradParameters},
   {NULL, NULL}
 };
 
