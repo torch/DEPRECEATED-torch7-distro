5fd512c0f48bbf7f6f20378ef060f8748746934c
Clement Farabet <clement.farabet@gmail.com>
2011-09-20 13:18:31 -0400

    Fixed C-style comment.

--- libraries/TH/THVector.h
+++ libraries/TH/THVector.h
@@ -227,13 +227,13 @@
 
 #else
 
-// If SSE2 not defined, then generate plain C operators
+/* If SSE2 not defined, then generate plain C operators */
 #include "generic/THVector.c"
 #include "THGenerateFloatTypes.h"
 
 #endif
 
-// For non-float types, generate plain C operators
+/* For non-float types, generate plain C operators */
 #include "generic/THVector.c"
 #include "THGenerateIntTypes.h"
 
