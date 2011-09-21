0c03b01f6bbedd51ff4d10eeee0174f083a88c5a
Ronan Collobert <ronan@collobert.com>
2011-09-21 10:18:08 +0200

    Removed TemporalLogSoftMax (now supported by LogSoftMax)

--- cuda/pkg/cunn/init.cu
+++ cuda/pkg/cunn/init.cu
@@ -12,7 +12,6 @@ const void *torch_CudaTensor_id = NULL;
 #include "HardTanh.cu"
 #include "Tanh.cu"
 #include "LogSoftMax.cu"
-#include "TemporalLogSoftMax.cu"
 #include "TemporalConvolution.cu"
 #include "SpatialConvolution.cu"
 #include "SpatialSubSampling.cu"
@@ -26,7 +25,6 @@ DLL_EXPORT TH_API int luaopen_libcunn(lua_State *L)
   cunn_Tanh_init(L);
   cunn_HardTanh_init(L);
   cunn_LogSoftMax_init(L);
-  cunn_TemporalLogSoftMax_init(L);
   cunn_TemporalConvolution_init(L);
   cunn_SpatialConvolution_init(L);
   cunn_SpatialSubSampling_init(L);
