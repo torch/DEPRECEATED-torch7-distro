a7f00033f8c48faaff8a8e8f21ed5d22b414f910
Clement Farabet <clement.farabet@gmail.com>
2011-09-24 12:02:07 -0400

    Added type() functions to cutorch.

--- cuda/pkg/cutorch/Tensor.lua
+++ cuda/pkg/cutorch/Tensor.lua
@@ -3,3 +3,35 @@ function torch.CudaTensor.apply(self, func)
    x:apply(func)
    self:copy(x)
 end
+
+local function Tensor__type(self,type)
+   local current = torch.typename(self)
+   if not type then return current end
+   if type ~= current then
+      local new = torch.getmetatable(type).new()
+      if self:nElement() > 0 then
+         new:resize(self:size()):copy(self)
+      end
+      return new
+   else
+      return self
+   end
+end
+
+local function Tensor__cuda(self,type)
+   return self:type('torch.CudaTensor')
+end
+local function Tensor__double(self,type)
+   return self:type('torch.DoubleTensor')
+end
+local function Tensor__float(self,type)
+   return self:type('torch.FloatTensor')
+end
+
+rawset(torch.getmetatable('torch.DoubleTensor'), 'cuda', Tensor__cuda)
+rawset(torch.getmetatable('torch.FloatTensor'), 'cuda', Tensor__cuda)
+rawset(torch.getmetatable('torch.CudaTensor'), 'cuda', Tensor__cuda)
+
+rawset(torch.getmetatable('torch.CudaTensor'), 'type', Tensor__type)
+rawset(torch.getmetatable('torch.CudaTensor'), 'double', Tensor__double)
+rawset(torch.getmetatable('torch.CudaTensor'), 'float', Tensor__float)
