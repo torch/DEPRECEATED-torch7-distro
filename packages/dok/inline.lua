62b13cd74156968016795ae53357b035b4c59d80
Clement Farabet <clement.farabet@gmail.com>
2012-01-02 22:21:14 +0100

    A couple of modifications to deal with import.
    It makes everything a bit complex... but it's worth it.

--- packages/dok/inline.lua
+++ packages/dok/inline.lua
@@ -52,7 +52,11 @@ end
 paths.install_dok = paths.concat(paths.install_hlp, '..', 'dok')
 
 local function string2symbol(str)
-   return loadstring('local t = ' .. str .. '; return t')()
+   local ok, res = pcall(loadstring('local t = ' .. str .. '; return t'))
+   if not ok then
+      ok, res = pcall(loadstring('local t = _torchimport.' .. str .. '; return t'))
+   end
+   return res
 end
 
 local function maxcols(str, cols)
@@ -137,28 +141,30 @@ function dok.refresh()
                   local content = f:read('*all')
                   local html = dok.dok2html(content)
                   local funcs = dok.html2funcs(html, package)
-                  if type(_G[package]) == 'table' then
-                     for key,symb in pairs(_G[package]) do
-                        -- level 1: global functions and objects
-                        local entry = (key):lower()
-                        if funcs[entry] then
-                           dok.inline[string2symbol(package .. '.' .. key)] = funcs[entry]
+                  local pkg = _G[package]
+                  if type(pkg) ~= 'table' then -- unsafe import, use protected import
+                     pkg = _G._torchimport[package]
+                  end
+                  for key,symb in pairs(pkg) do
+                     -- level 1: global functions and objects
+                     local entry = (key):lower()
+                     if funcs[entry] then
+                        dok.inline[string2symbol(package .. '.' .. key)] = funcs[entry]
+                     end
+                     -- level 2: objects' methods
+                     if type(pkg[key]) == 'table' then
+                        local entries = {}
+                        for k,v in pairs(pkg[key]) do
+                           entries[k] = v
                         end
-                        -- level 2: objects' methods
-                        if type(_G[package][key]) == 'table' then
-                           local entries = {}
-                           for k,v in pairs(_G[package][key]) do
-                              entries[k] = v
-                           end
-                           local mt = getmetatable(_G[package][key]) or {}
-                           for k,v in pairs(mt) do
-                              entries[k] = v
-                           end
-                           for subkey,subsymb in pairs(entries) do
-                              local entry = (key .. '.' .. subkey):lower()
-                              if funcs[entry] then
-                                 dok.inline[string2symbol(package .. '.' .. key .. '.' .. subkey)] = funcs[entry]
-                              end
+                        local mt = getmetatable(pkg[key]) or {}
+                        for k,v in pairs(mt) do
+                           entries[k] = v
+                        end
+                        for subkey,subsymb in pairs(entries) do
+                           local entry = (key .. '.' .. subkey):lower()
+                           if funcs[entry] then
+                              dok.inline[string2symbol(package .. '.' .. key .. '.' .. subkey)] = funcs[entry]
                            end
                         end
                      end
