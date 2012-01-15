752abca1b30fe4a11947cdf46ce1d397f073a478
koray kavukcuoglu <koray@kavukcuoglu.org>
2012-01-14 23:25:13 -0500

    nn documentations

--- packages/nn/generic/HardShrink.c
+++ packages/nn/generic/HardShrink.c
@@ -12,7 +12,7 @@ static int nn_(HardShrink_updateOutput)(lua_State *L)
   
   TH_TENSOR_APPLY2(real, output, real, input,				\
                    if ((*input_data) > lambda) *output_data = *input_data; \
-                   else if ((*input_data) < lambda) *output_data = *input_data; \
+                   else if ((*input_data) < -lambda) *output_data = *input_data; \
                    else *output_data = 0;);
   return 1;
 }
@@ -26,7 +26,7 @@ static int nn_(HardShrink_updateGradInput)(lua_State *L)
 
   THTensor_(resizeAs)(gradInput, input);
   TH_TENSOR_APPLY3(real, gradInput, real, gradOutput, real, input,	\
-                   if ((*input_data) > lambda || (*input_data) < lambda) \
+                   if ((*input_data) > lambda || (*input_data) < -lambda) \
 		     *gradInput_data = (*gradOutput_data);		\
 		   else							\
 		     *gradInput_data = 0;				\
