fffee08455d0fa8ddcb7864e49893dea75a6e5e4
Clement Farabet <clement.farabet@gmail.com>
2012-01-15 14:23:12 -0500

    Search now works for all subpackages

--- packages/dok/search.lua
+++ packages/dok/search.lua
@@ -2,22 +2,42 @@
 -- search in help
 -- that file defines all the tools and goodies to allow search
 --------------------------------------------------------------------------------
-dok.entries = {}
+local entries = {}
 
 paths.install_dok = paths.concat(paths.install_html, '..', 'dok')
 paths.install_dokmedia = paths.concat(paths.install_html, '..', 'dokmedia')
 
 local function html2entries(html, package, file)
-   local funcs = {}
    local next = html:gfind('<div.->\n<h%d><a.->%s+(.-)%s+</a></h%d><a.-></a>\n<a name="(.-)"></a>\n(.-)</div>')
    for title,link,body in next do
       link = package .. '/' .. file:gsub('.txt','.html') .. '#' .. link
       body = body:gsub('<img.->','')
-      table.insert(dok.entries, {title, link, body})
+      entries.global = entries.global or {}
+      table.insert(entries.global, {title, link, body})
+      entries[package] = entries[package] or {}
+      table.insert(entries[package], {title, '../' .. link, body})
+   end
+   return entries
+end
+
+local function install(entries, dir)
+   local vars = {}
+   for i,entry in ipairs(entries) do
+      table.insert(vars, 's[' .. (i-1) .. '] = "' 
+                .. table.concat(entry, '^'):gsub('"','\\"'):gsub('\n',' ') .. '";')
+   end
+   local array = table.concat(vars, '\n')
+   local f = paths.concat(paths.install_html, paths.basename(dir), 'jse_form.js')
+   if paths.filep(f) then
+      local js = io.open(f):read('*all')
+      js = js:gsub('// SEARCH_ARRAY //', array)
+      local w = io.open(f,'w')
+      w:write(js)
+      w:close()
    end
 end
 
-function dok.gensearch()
+function dok.installsearch()
    for package in paths.files(paths.install_dok) do
       if package ~= '.' and package ~= '..' then
          local dir = paths.concat(paths.install_dok, package)
@@ -28,26 +48,15 @@ function dok.gensearch()
                if f then
                   local content = f:read('*all')
                   local html = dok.dok2html(content)
-                  html2entries(html, package, file)
+                  local entries = html2entries(html, package, file)
                end
             end
          end
       end
    end
-end
-
-function dok.installsearch()
-   dok.gensearch()
-   local vars = {}
-   for i,entry in ipairs(dok.entries) do
-      table.insert(vars, 's[' .. (i-1) .. '] = "' 
-                   .. table.concat(entry, '^'):gsub('"','\\"'):gsub('\n',' ') .. '";')
+   for package,entries in pairs(entries) do
+      print('installing search for package: ' .. package)
+      if package == 'global' then package = '.' end
+      install(entries, package)
    end
-   local array = table.concat(vars, '\n')
-   local f = paths.concat(paths.install_html, 'jse_form.js')
-   local js = io.open(f):read('*all')
-   js = js:gsub('// SEARCH_ARRAY //', array)
-   local w = io.open(f,'w')
-   w:write(js)
-   w:close()
 end
