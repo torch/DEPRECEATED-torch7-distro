a882a8df6407ace99b8d7154a39af2a4d7c07700
Ronan Collobert <ronan@collobert.com>
2012-02-02 14:49:14 +0100

    cmake cleanup + include fix

--- cmake/TorchLua2Exe.cmake
+++ cmake/TorchLua2Exe.cmake
@@ -1,8 +1,7 @@
 MACRO(ADD_TORCH_LUA2EXE lua_exe lua_file)
 
-  FIND_PACKAGE(Lua REQUIRED)
   GET_FILENAME_COMPONENT(file_name ${lua_file} NAME_WE)
-  INCLUDE_DIRECTORIES(${LUA_INCLUDE_DIR} ${Torch_SOURCE_INCLUDES})
+  INCLUDE_DIRECTORIES(${Torch_SOURCE_INCLUDES})
 
   ADD_CUSTOM_COMMAND(
     OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${file_name}.c"
@@ -10,9 +9,9 @@ MACRO(ADD_TORCH_LUA2EXE lua_exe lua_file)
     ARGS "${Torch_SOURCE_CMAKE}/lua2exe/lua2exe.lua" "${lua_file}" "${CMAKE_CURRENT_BINARY_DIR}/${file_name}.c"
     DEPENDS "${lua_file}" "${Torch_SOURCE_LUA}" "${Torch_SOURCE_CMAKE}/lua2exe/lua2exe.lua")
 
-  IF(LUA_JIT AND APPLE)
+  IF(WITH_LUA_JIT AND APPLE)
     SET(CMAKE_EXE_LINKER_FLAGS "-pagezero_size 10000 -image_base 100000000 ${CMAKE_EXE_LINKER_FLAGS}")
-  ENDIF(LUA_JIT AND APPLE)
+  ENDIF(WITH_LUA_JIT AND APPLE)
   
   ADD_EXECUTABLE("${lua_exe}" "${CMAKE_CURRENT_BINARY_DIR}/${file_name}.c")
   TARGET_LINK_LIBRARIES("${lua_exe}" liblua-shared)
