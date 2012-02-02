89d00b50fcebe17359c4816e4ffd4529f51240e6
Ronan Collobert <ronan@collobert.com>
2012-02-02 12:40:27 +0100

    forgot to add Lua2Exe cmake file

---
+++ cmake/TorchLua2Exe.cmake
@@ -0,0 +1,20 @@
+MACRO(ADD_TORCH_LUA2EXE lua_exe lua_file)
+
+  FIND_PACKAGE(Lua REQUIRED)
+  GET_FILENAME_COMPONENT(file_name ${lua_file} NAME_WE)
+  INCLUDE_DIRECTORIES(${LUA_INCLUDE_DIR} ${Torch_SOURCE_INCLUDES})
+  MESSAGE("WTF: ${CMAKE_CURRENT_BINARY_DIR}")
+  ADD_CUSTOM_COMMAND(
+    OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${file_name}.c"
+    COMMAND "${Torch_SOURCE_LUA}"
+    ARGS "${Torch_SOURCE_CMAKE}/lua2exe/lua2exe.lua" "${lua_file}" "${CMAKE_CURRENT_BINARY_DIR}/${file_name}.c"
+    DEPENDS "${lua_file}" "${Torch_SOURCE_LUA}" "${Torch_SOURCE_CMAKE}/lua2exe/lua2exe.lua")
+
+  IF(LUA_JIT AND APPLE)
+    SET(CMAKE_EXE_LINKER_FLAGS "-pagezero_size 10000 -image_base 100000000 ${CMAKE_EXE_LINKER_FLAGS}")
+  ENDIF(LUA_JIT AND APPLE)
+  
+  ADD_EXECUTABLE("${lua_exe}" "${CMAKE_CURRENT_BINARY_DIR}/${file_name}.c")
+  TARGET_LINK_LIBRARIES("${lua_exe}" liblua-shared)
+
+ENDMACRO(ADD_TORCH_LUA2EXE lua_exe lua_file)
