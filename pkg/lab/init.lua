fa01a96b17af718be999ea5bae9ee8f679d62e3f
Ronan Collobert <ronan@collobert.com>
2012-02-04 20:06:21 +0100

    removed random+lab [+forgot to modify a filename in cutorch CMakeLists.txt]

--- pkg/lab/init.lua
+++
@@ -1,7 +0,0 @@
-require 'torch'
-
-print("***** WARNING: the 'lab' package is deprecated. Please use 'torch' package instead *****")
-
-lab = torch
-
-return lab
--- pkg/random/init.lua
+++
@@ -1,7 +0,0 @@
-require 'torch'
-
-print("***** WARNING: the 'random' package is deprecated. Please use 'torch' package instead *****")
-
-random = torch
-
-return random
