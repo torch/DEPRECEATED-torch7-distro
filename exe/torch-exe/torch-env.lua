6b1e23f209a3fc716da58ff8e6d5d7778fee041f
Ronan Collobert <ronan@collobert.com>
2012-02-10 11:03:32 +0100

    moved local path setup into torch-env.lua

--- exe/torch-exe/torch-env.lua
+++ exe/torch-exe/torch-env.lua
@@ -183,3 +183,12 @@ end
 import 'torch'
 import 'gnuplot'
 import 'dok'
+
+-- setup local paths
+local localinstalldir = paths.concat(os.getenv('HOME'),'.torch','usr')
+if paths.dirp(localinstalldir) then
+   package.path = paths.concat(localinstalldir,'share','torch','lua','?','init.lua') .. ';' .. package.path
+   package.path = paths.concat(localinstalldir,'share','torch','lua','?.lua') .. ';' ..  package.path
+   package.cpath = paths.concat(localinstalldir,'lib','torch','?.so') .. ';' .. package.cpath
+   package.cpath = paths.concat(localinstalldir,'lib','torch','?.dylib') .. ';' .. package.cpath
+end
