6f4b2ac85801d0e1308f9cbbea83745d8774e65b
Julien Cornebise <julien.cornebise@gmail.com>
2013-10-12 17:46:10 +0100

    Allow pngcairo if available
    This provides nicer-looking png

--- pkg/gnuplot/gnuplot.lua
+++ pkg/gnuplot/gnuplot.lua
@@ -691,7 +691,8 @@ function gnuplot.figprint(fname)
    if suffix == 'eps' then
       term = 'postscript eps enhanced color'
    elseif suffix == 'png' then
-      term = 'png size "1024,768"'
+      term = gnuplothasterm('pngcairo') and 'pngcairo' or 'png'
+      term = term .. ' size "1024,768"'
    elseif suffix == 'pdf' and haspdf then
       if not haspdf then
           error('your installation of gnuplot does not have pdf support enabled')
