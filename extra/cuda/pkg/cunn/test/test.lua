501abf7ac6834b88fb81a60f1f1872099424b96e
Jonathan Tompson <tompson@cims.nyu.edu>
2013-11-06 18:16:13 -0500

    added cuda support for nn.Abs (and added test for it).

--- extra/cuda/pkg/cunn/test/test.lua
+++ extra/cuda/pkg/cunn/test/test.lua
@@ -105,6 +105,71 @@ function cunntest.Tanh_backward()
    mytester:assertlt(error:abs():max(), precision_backward, 'error on state (backward) ')
 end
 
+function cunntest.Abs_forward()
+   local size = math.random(1,100)
+
+   local tm = {}
+   local title = string.format('Abs forward %d -> %d', size, size)
+   times[title] = tm
+
+   local input = torch.randn(size)
+   local sconv = nn.Abs()
+   local groundtruth = sconv:forward(input)
+   local a = torch.Timer()
+   for i = 1,nloop do
+      groundtruth = sconv:forward(input)
+   end
+   tm.cpu = a:time().real
+
+   input = input:cuda()
+   local gconv = nn.Abs():cuda()
+   local rescuda = gconv:forward(input)
+   a:reset()
+   for i = 1,nloop do
+      rescuda = gconv:forward(input)
+   end
+   cutorch.synchronize()
+   tm.gpu = a:time().real
+
+   local error = rescuda:float() - groundtruth
+   mytester:assertlt(error:abs():max(), precision_forward, 'error on state (forward) ')
+end
+
+function cunntest.Abs_backward()
+   local size = math.random(1,100)
+
+   local tm = {}
+   local title = string.format('Abs.backward %d -> %d', size, size)
+   times[title] = tm
+
+   local input = torch.randn(size)
+   local gradOutput = torch.randn(size)
+   local sconv = nn.Abs()
+   sconv:forward(input)
+   local groundgrad = sconv:backward(input, gradOutput)
+   local a = torch.Timer()
+   for i = 1,nloop do
+      groundgrad = sconv:backward(input, gradOutput)
+   end
+   tm.cpu = a:time().real
+
+   input = input:cuda()
+   gradOutput = gradOutput:cuda()
+   local gconv = nn.Abs():cuda()
+   gconv:forward(input)
+   local rescuda = gconv:backward(input, gradOutput)
+   a:reset()
+   for i = 1,nloop do
+      rescuda = gconv:backward(input, gradOutput)
+   end
+   cutorch.synchronize()
+   tm.gpu = a:time().real
+
+   local error = rescuda:float() - groundgrad
+
+   mytester:assertlt(error:abs():max(), precision_backward, 'error on state (backward) ')
+end
+
 function cunntest.Sigmoid_forward()
    local size = math.random(1,100)
 
